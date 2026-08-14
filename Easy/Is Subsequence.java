8b37671f284af5ab1da70f12c853f4850cbd0bfb
Rishi Patel <patelrishi297970@gmail.com>
2026-08-14 09:00:53 +0530

    Solved: Is Subsequence
    Difficulty: Easy
    Language: Java

--- Easy/Is Subsequence.java
+++ Easy/Is Subsequence.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        
+             // Edge cases
+        if (s == null || s.isEmpty()) {
+            return true;
+        }
+
+        if (t == null || t.isEmpty()) {
+            return false;
+        }
+
+        // Optional: if s is longer, it cannot be a subsequence
+        if (s.length() > t.length()) {
+            return false;
+        }
 
         int i = 0, j = 0;
 
@@ -11,6 +23,4 @@ class Solution {
             j++;
         }
 
-        return i == s.length();
-    }
-}
+      
