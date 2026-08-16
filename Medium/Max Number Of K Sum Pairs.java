7005006208684454506d51c965b8f2437479bb5b
Rishi Patel <patelrishi297970@gmail.com>
2026-08-16 10:37:24 +0530

    Solved: Max Number Of K Sum Pairs
    Difficulty: Medium
    Language: Java

---
+++ Medium/Max Number Of K Sum Pairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length - 1;
+        int operations = 0;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+
+            if (sum == k) {
+                operations++;
+                left++;
+                right--;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+ 
