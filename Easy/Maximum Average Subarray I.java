89707c9ed583eb6e9cecb94b2c20d3aaf4ea6640
Rishi Patel <patelrishi297970@gmail.com>
2026-08-17 19:35:25 +0530

    Solved: Maximum Average Subarray I
    Difficulty: Easy
    Language: Java

---
+++ Easy/Maximum Average Subarray I.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        if(nums.length==1) return (double)nums[0];
+        double maxSum=0;
+        double sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        maxSum=sum;
+
+        for(int i=k;i<nums.length;i++){
+            sum+=nums[i]-nums[i-k];
+            maxSum=Math.max(maxSum,sum);
+        }
+
+        return maxSum/k;
+    }
+}
