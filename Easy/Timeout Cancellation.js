15a9465d3965744f125f998c847a583296d2fc3c
Rishi Patel <patelrishi297970@gmail.com>
2026-08-31 09:41:39 +0530

    Solved: Timeout Cancellation
    Difficulty: Easy
    Language: JavaScript

---
+++ Easy/Timeout Cancellation.js
@@ -0,0 +1,10 @@
+var cancellable = function(fn, args, t) {
+    const timer = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+    return function cancelFn() {
+        clearTimeout(timer);
+    };  
+};
+ 
