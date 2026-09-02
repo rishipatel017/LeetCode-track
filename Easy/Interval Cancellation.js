8158656a45eeb3174349fec28044cc6937fddde4
Rishi Patel <patelrishi297970@gmail.com>
2026-09-02 11:39:12 +0530

    Solved: Interval Cancellation
    Difficulty: Easy
    Language: JavaScript

---
+++ Easy/Interval Cancellation.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+     // Call immediately at time 0 
+     fn(...args);
+      // Then call every t milliseconds 
+      const intervalId = setInterval(() => { fn(...args); }, t); 
+      // Function to cancel future calls 
+      return function cancelFn() { clearInterval(intervalId); };
+
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *   
+ *  setTimeout((
